d45ddb03fd984117ee117303ed21d97b60da7339
Software Engineer Alan Guerra <esalanguerra@gmail.com>
2024-11-12 06:44:24 -0300

    alfabeto brasileiro

---
+++ brazilian_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Exibindo o alfabeto brasileiro completo
+    printf("Alfabeto Brasileiro:\n");
+    printf("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z\n");
+    printf("a b c d e f g h i j k l m n o p q r s t u v w x y z\n");
+    printf("Á É Í Ó Ú Â Ê Ô Ã Õ Ç\n");
+    printf("á é í ó ú â ê ô ã õ ç\n");
+
+    return 0;
+}
+
